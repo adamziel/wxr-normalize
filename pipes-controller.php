daaba8abad2d3966c83d8e8aead3b9a0a0e27b8a
Adam Zieliński <adam@adamziel.com>
2024-08-27 10:31:07 +0200

    A loop-based API without nested loops

--- pipes-controller.php
+++ pipes-controller.php
@@ -629,20 +629,20 @@ $chain = new StreamChain(
 // var_dump([$chain->next_chunk(), strlen($chain->get_bytes()), $chain->get_last_error()]);
 
 // Or like this:
-// $chain->stop_on_errors(true);
-// foreach($chain as $chunk) {
-//     switch($chunk->get_chunk_type()) {
-//         case '#error':
-//             echo "Error: " . $chunk->get_last_error() . "\n";
-//             break;
-//         case '#bytes':
-//             var_dump([
-//                 $chunk->get_bytes(),
-//                 'zip file_id' => isset($chain['zip']) ? $chain['zip']->get_file_id() : null
-//             ]);
-//             break;
-//     }
-// }
+$chain->stop_on_errors(true);
+foreach($chain as $chunk) {
+    switch($chunk->get_chunk_type()) {
+        case '#error':
+            echo "Error: " . $chunk->get_last_error() . "\n";
+            break;
+        case '#bytes':
+            var_dump([
+                $chunk->get_bytes(),
+                'zip file_id' => isset($chain['zip']) ? $chain['zip']->get_file_id() : null
+            ]);
+            break;
+    }
+}
 
 
 // ----------------------------------------------------------
@@ -658,63 +658,73 @@ $client->enqueue([
 $zip_readers = [];
 $xml_processors = [];
 $xml_tokens_found = [];
-while ($client->await_next_event()) {
-    // Fetch HTTP data
-    $request = $client->get_request();
-    switch ($client->get_event()) {
-        case Client::EVENT_BODY_CHUNK_AVAILABLE:
-            // Continue to the next stage
+
+$chunks = [];
+while(true) {
+    if(empty($chunks)) {
+        $event = $client->await_next_event();
+        if(false === $event) {
             break;
-        case Client::EVENT_FAILED:
-            error_log('Request failed: ' . $request->error);
-        default:
-            continue 2;
+        }
+        $chunks[] = ['http', null];
     }
 
-    // Unzip the file
-    $zip_reader = $zip_readers[$request->id] ?? new ZipStreamReader();
-    $zip_reader->append_bytes($client->get_response_body_chunk());
-    while ($zip_reader->next()) {
-        switch ($zip_reader->get_state()) {
-            case ZipStreamReader::STATE_FILE_ENTRY:
-                // Continue to the next stage
-                break;
-            default:
+    list($stage, $chunk) = array_pop($chunks);
+
+    switch ($stage) {
+        case 'http':
+            $request = $client->get_request();
+            switch ($client->get_event()) {
+                case Client::EVENT_BODY_CHUNK_AVAILABLE:
+                    $chunks[] = ['zip', $client->get_response_body_chunk()];
+                    break;
+                case Client::EVENT_FAILED:
+                    error_log('Request failed: ' . $request->error);
+                    break;
+            }
+            break;
+        case 'zip':
+            $zip_reader = $zip_readers[$request->id] ?? new ZipStreamReader();
+            $zip_reader->append_bytes($chunk);
+            while ($zip_reader->next()) {
+                switch ($zip_reader->get_state()) {
+                    case ZipStreamReader::STATE_FILE_ENTRY:
+                        if ($zip_reader->get_file_path() === 'export.wxr') {
+                            continue 2;
+                        }
+                        $chunks[] = ['xml', $zip_reader->get_file_body_chunk()];
+                        break;
+                }
+            }
+            break;
+        case 'xml':
+            $xml_processor = $xml_processors[$request->id] ?? new WP_XML_Processor('', [], WP_XML_Processor::IN_PROLOG_CONTEXT);
+            $xml_processor->append_bytes($chunk);
+
+            $xml_tokens_found[$request->id] ??= 0;
+            while ($xml_processor->next_token()) {
+                ++$xml_tokens_found[$request->id];
+                // Process the XML
+            }
+
+            $buffer = '';
+            if ($xml_tokens_found[$request->id] > 0) {
+                $buffer .= $xml_processor->get_updated_xml();
+            } else if (
+                $xml_tokens_found[$request->id] === 0 &&
+                !$xml_processor->is_paused_at_incomplete_input() &&
+                $xml_processor->get_current_depth() === 0
+            ) {
+                // We've reached the end of the document, let's finish up.
+                $buffer .= $xml_processor->get_unprocessed_xml();
+            }
+
+            if (!strlen($buffer)) {
                 continue 2;
-        }
+            }
 
-        if($zip_reader->get_file_path() === 'export.wxr') {
-            continue;
-        }
-
-        // Process the XML
-        $xml_processor = $xml_processors[$request->id] ?? new WP_XML_Processor('', [], WP_XML_Processor::IN_PROLOG_CONTEXT);
-        $xml_processor->append_bytes($zip_reader->get_file_body_chunk());
-
-        $xml_tokens_found[$request->id] ??= 0;
-        while ($xml_processor->next_token()) {
-            ++$xml_tokens_found[$request->id];
-            // Process the XML
-        }
-
-        $buffer = '';
-        if ($xml_tokens_found[$request->id] > 0) {
-            $buffer .= $xml_processor->get_updated_xml();
-        } else if (
-            $xml_tokens_found[$request->id] === 0 &&
-            !$xml_processor->is_paused_at_incomplete_input() &&
-            $xml_processor->get_current_depth() === 0
-        ) {
-            // We've reached the end of the document, let's finish up.
-            // @TODO: Fix this so it doesn't return the entire XML
-            $buffer .= $xml_processor->get_unprocessed_xml();
-        }
-
-        if (!strlen($buffer)) {
-            continue;
-        }
-
-        // Uppercase the output
-        echo strtoupper($buffer);
+            // Uppercase the output
+            echo strtoupper($buffer);
+            break;
     }
 }
